f0ace12de05eb76a899a87b00c3e6330eca3206b
HorroGlitch <lverot@edenschool.fr>
2024-03-05 15:31:53 +0100

    adding css and js modification

--- main.js
+++ main.js
@@ -1,3 +1,45 @@
 import './style.css'
 import './about.css'
 import './contact.css'
+
+const crossPlusButton = document.querySelectorAll(".crossPlusButton");
+const crossLessButton = document.querySelectorAll(".crossLessButton")
+const reversedCard = document.querySelectorAll(".reversedCard");
+const profile = document.querySelectorAll(".profile")
+
+const nav = document.getElementsByClassName("burgerNav");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for (let i = 0; i < 3; i++) {
+//     console.log(crossPlusButton[i])
+    
+//     crossPlusButton[i].addEventListener("click", ()=>{
+   
+//         document.querySelector(".profile").style.display="none"
+//         document.querySelector(".reversedCard").style.display="flex"
+ 
+//     })
+
+//     crossLessButton[i].addEventListener("click", ()=>{
+
+//         document.querySelector(".profile").style.display="flex"
+//         document.querySelector(".reversedCard").style.display="none"
+    
+//     })
+    
+// }
+
+
+        
